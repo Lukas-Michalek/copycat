af3fa0ab884677eac733f54343778d0b2dc0113c
Luke <luke.michalek@gmail.com>
2023-01-07 20:16:15 +0000

    Initial Version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import CopyCat from './Components/CopyCat/CopyCat';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CopyCat />
   </React.StrictMode>
 );
 
